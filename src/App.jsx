066b0aeda9f07d8070485b2bd0fd592961892c3e
Ajay Prasad <ajay.prasad@outlook.in>
2023-09-22 14:24:51 +0530

    todo app completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import './App.css'
+import Todos from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -6,6 +9,8 @@ function App() {
   return (
     <>
       <h6 className='text-white m-auto text-center' >Redux toolkit</h6>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
